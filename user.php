51e5369298c48e6662e074f6a038c8702de4d8b2
Malikabhc <malika.bouhcine@gmail.com>
2018-08-02 12:01:09 +0200

    exercice2-7

---
+++ user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice2/7 User</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        //Le formulaire renvoie à cette page user.php
+      ?>
+    </p>
+  </body>
+</html>
